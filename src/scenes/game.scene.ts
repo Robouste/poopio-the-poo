6817dfad1c8da7220e2e027f4248949c6a14da19
Robouste <robindenis88@gmail.com>
2024-12-29 19:32:52 +0100

    - tuto

--- src/scenes/game.scene.ts
+++ src/scenes/game.scene.ts
@@ -8,7 +8,20 @@ export class GameScene {
 
 		bean.init();
 
-		// platform
+		this.addPlatform();
+		this.spawnTree();
+		this.addHowToPlay();
+	}
+
+	private addHowToPlay(): void {
+		add([
+			text("Space to jump. Space again to double jump"),
+			pos(center()),
+			anchor("center"),
+		]);
+	}
+
+	private addPlatform(): void {
 		add([
 			rect(width(), 48),
 			pos(0, height() - 48),
@@ -18,24 +31,22 @@ export class GameScene {
 			color(127, 200, 250),
 			"ground",
 		]);
+	}
 
-		const spawnTree = () => {
-			add([
-				rect(48, rand(24, 64)),
-				area(),
-				outline(4, new Color(0, 0, 0)),
-				pos(width(), height() - 48),
-				anchor("botleft"),
-				color(255, 180, 255),
-				move(LEFT, 500),
-				"tree",
-			]);
+	private spawnTree(): void {
+		add([
+			rect(48, rand(24, 64)),
+			area(),
+			outline(4, new Color(0, 0, 0)),
+			pos(width(), height() - 48),
+			anchor("botleft"),
+			color(255, 180, 255),
+			move(LEFT, 500),
+			"tree",
+		]);
 
-			wait(rand(0.5, 1.5), () => {
-				spawnTree();
-			});
-		};
-
-		spawnTree();
+		wait(rand(0.5, 1.5), () => {
+			this.spawnTree();
+		});
 	}
 }
